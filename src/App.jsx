d78a87fd7d2381e5cf80c4efcad0147a54baf2c4
Anirudha <beleanirudha8@gmail.com>
2024-08-26 12:56:23 +0530

    add styling using tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
 
 function App() {
-	const [name, setName] = useState("Anirudha");
 	return (
 		<>
-			<div>{name}</div>
-			<button onClick={() => setName("Anirudha Bele")}>Update</button>
+			<header className="bg-green-500 font-semibold text-white px-8 py-6 flex items-center justify-between gap-y-2 flex-wrap">
+				<button className="bg-zinc-900 p-2 rounded">LOGO</button>
+				<nav className="text-xl flex list-none gap-x-10 gap-y-3 flex-wrap hover:*:text-zinc-900 ">
+					<li>
+						<a href="">Home</a>
+					</li>
+					<li>
+						<a href="">About</a>
+					</li>
+					<li>
+						<a href="">Contact</a>
+					</li>
+					<li>
+						<a href="">Help</a>
+					</li>
+				</nav>
+			</header>
 		</>
 	);
 }
